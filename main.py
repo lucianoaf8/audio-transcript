190e4d2a0e70fa1240978d2730c8773d5d4179b8
Luciano A <lucianoaf8@gmail.com>
2024-06-06 22:30:23 -0600

    path fix

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ import whisper
 
 model = whisper.load_model("base")
 
-input_dir = "C:/Users/Luciano/Documents/Projects/audio_to_text/to_transcript"
-output_dir = "C:/Users/Luciano/Documents/Projects/audio_to_text/transcripted"
+input_dir = r"C:\Users\Luciano\Documents\Projects\audio-transcript\to_transcript"
+output_dir = r"C:\Users\Luciano\Documents\Projects\audio-transcript\transcripted"
 
+# Ensure directories exist
+if not os.path.exists(input_dir):
+    os.makedirs(input_dir)
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
 for filename in os.listdir(input_dir):
-    if filename.endswith(".mp3") or filename.endswith(".wav"):
+    if filename.endswith(".m4a"):
         input_path = os.path.join(input_dir, filename)
         output_path = os.path.join(output_dir, os.path.splitext(filename)[0] + ".txt")
 
